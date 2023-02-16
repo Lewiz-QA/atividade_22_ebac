8c3b1f4dbd64186409907c1805dcb1f3eee9c258
Luiz <lgm97.com@gmail.com>
2023-02-16 01:45:14 -0300

    Atividade 22 Cypress

--- cypress/integration/Register.cy.js
+++ cypress/integration/Register.cy.js
@@ -1,20 +1,28 @@
 /// <reference types="cypress" />
 
-const {accountPage, registerPage} = require('../support/pages')
+const { accountPage, registerPage } = require('../support/pages')
 
 var faker = require('faker');
 
-let emailFaker = faker.internet.email()
-let senhaFaker = faker.lorem.word(10)
-let username = emailFaker.substring(0, emailFaker.indexOf("@"))
+let mailFaker = faker.internet.email()
+let passFaker = faker.lorem.word(10)
+let username = mailFaker.substring(0, mailFaker.indexOf("@"))
 
-describe('Access Admin Panel', () => {
-    beforeEach(() => {
-        cy.visit('/minha-conta')
-    });
+describe('Register Account', () => {
 
-    it('Deve realizar o cadastro de uma conta', () => {
-        registerPage.register(emailFaker, senhaFaker)
-        accountPage.welcomeMessage.contains(username, { matchCase: false })
+    context('Given I visit the page Minha Conta of Ebac Store logged out', () => {
+        before(() => {
+            cy.visit('/minha-conta')
+        });
+
+        context(`When I register an account with mail ${mailFaker} and password ${passFaker}`, () => {
+            beforeEach(() => {
+                registerPage.register(mailFaker, passFaker)
+            });
+
+            it('Then should appear a welcome message in the logged area', () => {
+                accountPage.welcomeMessage.contains(username, { matchCase: false })
+            });
+        });
     });
 });
